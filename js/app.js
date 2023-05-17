4a43a82017a715f0eb67c29978dd7e1bb733fd35
daniaa98 <dania.ashraf98@gmail.com>
2023-05-17 07:25:14 +0000

    adding changes

---
+++ js/app.js
@@ -0,0 +1,34 @@
+'use strict';
+prompt (" enter your name ");
+let Gender = prompt (" enter your gender ") 
+    if (Gender === 'male') {
+        alert;
+    } else if (Gender === 'female') {
+        alert;
+    } else {
+        alert ( 'invalid');
+    }
+    
+let Age = prompt ("enter your age")
+if (Age >=0) {
+    alert;
+} else {
+    alert ( 'invalid age ');
+}
+confirm ("do you want to skip the welcoming message");
+
+const name = prompt('Enter your name:');
+const gender = prompt('Enter your gender');
+if (gender === 'male') {
+    alert ("welcome Mr " + name);
+} else if (gender === 'female') {
+    alert ("welcome Ms " + name);
+} else {
+    alert ( 'welcome');
+}
+
+confirm ("do you like playing sport ");
+confirm ("do you like reading books");
+confirm ("do you like watching movies");
+
+
