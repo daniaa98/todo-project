ca9686ab13eb63a258a1eda08f8edee3b0a118a5
daniaa98 <dania.ashraf98@gmail.com>
2023-05-20 22:23:42 +0000

    changes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
 'use strict';
-prompt (" enter your name ");
-let Gender = prompt (" enter your gender ") 
-    if (Gender === 'male') {
-        alert;
-    } else if (Gender === 'female') {
-        alert;
-    } else {
-        alert ( 'invalid');
-    }
-    
-let Age = prompt ("enter your age")
-if (Age >=0) {
-    alert;
-} else {
-    alert ( 'invalid age ');
+var userName = prompt("Enter your name:");
+var userGender;
+while (true) {
+  userGender = prompt("Enter your gender:");
+  if (userGender === "male" || userGender === "female") {
+    break; 
+  }
+  alert("Invalid ,Please enter either 'male' or 'female'.");
 }
-confirm ("do you want to skip the welcoming message");
+
+    
+ var Age;
+ while (true) {
+    Age=prompt ("enter your Age");
+    if (Age >0) {
+        break;
+    } 
+        alert ( 'invalid age,less than or equal to zero ');
+ }
+ confirm ("do you want to skip the welcoming message");
 
 const name = prompt('Enter your name:');
 const gender = prompt('Enter your gender');
@@ -25,10 +28,15 @@ if (gender === 'male') {
     alert ("welcome Ms " + name);
 } else {
     alert ( 'welcome');
+    console.log ("welcome ");
 }
 
-confirm ("do you like playing sport ");
-confirm ("do you like reading books");
-confirm ("do you like watching movies");
+var question1 = prompt ("do you like playing sport ");
+var question2 =prompt ("do you like reading books");
+var question3 =prompt ("do you like watching movies");
 
-
+var answers = [question1 || "invalid", question2 || "invalid", question3 || "invalid"];
+console.log("Answers:");
+    for (var i = 0; i < answers.length; i++) {
+      console.log(answers[i]);
+    }
